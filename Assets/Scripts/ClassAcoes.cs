279a0d583040c2c6f7f62c5a1dfcddab8f3135ea
DouglasBarbino <douglas-448@hotmail.com>
2018-09-10 10:00:07 -0300

    Implementação da classe Metodologias
    - Melhorias no arquivo metodologias.json
    - Classe das metodologias implementada, junto com seus filhos

---
+++ Assets/Scripts/ClassAcoes.cs
@@ -0,0 +1,5 @@
+﻿[System.Serializable]
+public class ClassAcoes{
+	private string id;
+	private string nome;
+}
---
+++ Assets/Scripts/ClassMetodologias.cs
@@ -0,0 +1,5 @@
+﻿[System.Serializable]
+public class ClassMetodologias{
+	private string tipo;
+	private ClassAcoes[] acoes;
+}
