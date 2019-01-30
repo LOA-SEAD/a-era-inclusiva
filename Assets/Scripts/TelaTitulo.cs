bc548dca8262bfa2c4c9347055ced0632a9d2616
João Pedro S. G. Silva <pedrosgregorio9@gmail.com>
2019-01-30 15:30:13 -0200

    Tela de titulo adicionada

---
+++ Assets/Scripts/TelaTitulo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TelaTitulo : MonoBehaviour
+{
+   public void IniciarJogo()
+    {
+        SceneManager.LoadScene("Scenes/MenuPrincipal");
+    }
+}
