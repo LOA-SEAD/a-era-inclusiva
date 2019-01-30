acd262d1bd86f9aa1598e4893d87bc76c6904cfb
João Pedro S. G. Silva <pedrosgregorio9@gmail.com>
2019-01-30 16:23:19 -0200

    Animações na tela de titulo

--- Assets/Scripts/TelaTitulo.cs
+++ Assets/Scripts/TelaTitulo.cs
@@ -5,8 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class TelaTitulo : MonoBehaviour
 {
-   public void IniciarJogo()
+    public GameObject botaoInvisivel;
+
+    private void Start()
     {
-        SceneManager.LoadScene("Scenes/MenuPrincipal");
+        animator = instrucoes.GetComponent<Animator>();
     }
+
+    public GameObject instrucoes;
+    public GameObject botoes;
+    private static readonly int Hiding = Animator.StringToHash("Hiding");
+    private Animator animator;
+    private bool menuVisible = false;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/ConversaAndre");
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && !menuVisible)
+        {
+            HideInstructionsAndShowMenu();
+        }
+    }
+
+    public void HideInstructionsAndShowMenu()
+    {
+        Destroy(botaoInvisivel);
+        animator.SetBool(Hiding, true);
+        Destroy(instrucoes, 1f);
+        botoes.SetActive(true);
+        menuVisible = true;
+    }
+
+ 
 }
