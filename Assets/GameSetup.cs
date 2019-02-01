4b136d390a675f2f5367f74c13bb36685c094dce
João Pedro S. G. Silva <pedrosgregorio9@gmail.com>
2019-02-01 17:37:13 -0200

    Inicio das variaveis globais e corredor

---
+++ Assets/GameSetup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public TextAsset JsonMetodologias;
+    public TextAsset JsonAlunos;
+    public TextAsset JsonFalas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+
+            JsonUtility.FromJsonOverwrite(JsonMetodologias.text, Game.Metodologias);
+       
+            Game.Alunos = JsonUtility.FromJson<List<ClassAlunos>>(JsonAlunos.text);
+            Game.Falas = JsonUtility.FromJson<List<ClassFalas>>(JsonFalas.text);
+        } catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Game
+{
+    // Start is called before the first frame update
+    public static List<ClassAlunos> Alunos;
+    public static List<ClassMetodologias> Metodologias;
+    public static List<ClassMetodologias> MetodologiasSelecionadas;
+    public static List<ClassFalas> Falas;
+
+    public static int Happiness; 
+
+    public static void Setup()
+    {
+
+        Happiness = 0;
+        Alunos = new List<ClassAlunos>();
+        Metodologias = new List<ClassMetodologias>();
+        MetodologiasSelecionadas = new List<ClassMetodologias>();
+        Falas = new List<ClassFalas>();
+    }
+}
