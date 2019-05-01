a4f51a277ee2748f7dd79428d94ccdc95cf700d7
João Pedro S. G. Silva <pedrosgregorio9@gmail.com>
2019-05-01 19:05:31 -0300

    Tentativa de adicionar uma lista mais acessivel

--- Assets/Scripts/ScrollList.cs
+++ Assets/Scripts/ScrollList.cs
@@ -10,9 +10,40 @@ public class ScrollList<T> : MonoBehaviour
     public Button up;
     public Button down;
     public List<GameObject> objects;
-     public int maxShown = 3;
      public Transform objectsParent;
-    private int _at;
+    private int selected;
+
+    public int Selected
+    {
+        get
+        {
+            if (selected < 0) return 0;
+            if (selected > objects.Count-1) return objects.Count()-1;
+            return selected;
+        }
+    
+        set
+        {
+            if (value < 0 || value > objects.Count-1)
+                return;
+            objects[Selected].GetComponent<Image>().color = (Color.white);
+            selected = value;
+            objects[Selected].GetComponent<Image>().color = (Color.black);
+
+            for (int i = 0; i < objects.Count; i++)
+            {
+                if (i >= Selected-1 && i <= Selected + 1)
+                {
+                    objects[i].SetActive(true);
+                }
+                else
+                {
+                    objects[i].SetActive(false);
+                }
+            }
+
+        }
+    }
 
     // Start is called before the first frame update
     protected void Awake()
@@ -30,7 +61,6 @@ public class ScrollList<T> : MonoBehaviour
         {
             objects = new List<GameObject>();
         }
-        UpdateShown();
     }
     public void AddGameObject(Transform obj)
     {
@@ -38,52 +68,28 @@ public class ScrollList<T> : MonoBehaviour
         obj.SetAsLastSibling();
         objects.Add(obj.gameObject);
         obj.localScale = Vector3.one;
+        Selected=objects.Count-1;
         
-        UpdateShown();
     }
 
 
-    protected void UpdateShown()
-    {
-        objects.ForEach(x=>x.SetActive(false));
-
-    
-            for (int id = _at; id < _at + maxShown && id<objects.Count; id++)
-            {
-                    objects[id].transform.localScale = Vector3.one;
-                    objects[id].SetActive(true);
-            }
-    }
 
     protected void GoUp()
     {
-        if (_at == 0)
-        {
-            return;
-        }
-        objects[_at+2].SetActive(false);
-        _at--;
-        objects[_at].SetActive(true);
+        Selected--;
 
     }
 
     protected void GoDown()
     {
-        if (_at + maxShown >= objects.Count)
-        {
-            return;
-        }
-        objects[_at].SetActive(false);
-        _at++;
-        objects[_at+2].SetActive(true);
+        Selected++;
 
-   
-       
+
     }
 
     public void Clear()
     {
-        _at = 0;
+        Selected = 0;
         objects.RemoveAll(x=>x);
         foreach (Transform children in objectsParent.transform)
         {
