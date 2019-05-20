323d24c0aa939074db086ff76f074d61a0baba05
Julia <juliamourac@hotmail.com>
2019-05-20 17:27:44 -0300

    Sala de Aula com os menus interagindo

---
+++ Assets/AcaoIcon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class AcaoIcon : Toggle
+{
+
+    private ClassAcao _acao;
+    public TextMeshProUGUI iconObj;
+   
+
+    public ClassAcao Acao
+    {
+        get { return _acao; }
+        set
+        {
+            _acao = value;
+            iconObj.SetText(_acao.icone);
+            nameObj.SetText(_acao.nome);
+            
+        }
+    }
+
+    public void Toggle()
+    {
+        _acao.ToggleSelection();
+    }
+
+   
+
+}
---
+++ Assets/Scripts/ActionListSalaDeAula.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionListSalaDeAula : ActionList
+{
+    protected override bool WhichActions(ClassAcao x)
+    {
+        return x.tipo == Type & x.selected;
+    }
+}
