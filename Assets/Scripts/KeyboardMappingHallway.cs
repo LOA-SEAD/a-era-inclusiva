1ddaad46179eec1eaf4eb76bb656d4dc16dfd83f
Julia <juliamourac@hotmail.com>
2019-07-10 14:25:04 -0300

    Scripts de controle pelo teclado adicionados

---
+++ Assets/Scripts/KeyboardMappingHallway.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardMappingHallway : MonoBehaviour
+{
+    public Transform[] optionReferences;
+    public Transform confirmation;
+    private int option;
+
+    private Transform confirmationMessage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {  
+        foreach (Transform child in confirmation.GetChild(0))
+        {
+            Debug.Log("Child: " +child.name+"\n");
+            if (child.gameObject.name.Equals("Message"))
+                confirmationMessage = child;
+        }
+        
+    }
+
+    // Update is called once per frame    
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            ShowMessage(0);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            ShowMessage(1);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            ShowMessage(2);  
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+           ShowMessage(3);
+        }
+
+        if (Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
+        {
+            if(confirmation.gameObject.activeInHierarchy)
+                GoToRoom();
+        }
+        if (Input.GetKeyDown("escape"))
+            confirmation.gameObject.SetActive(false);
+    }
+
+    void ChangeMessage(string message, Transform gameObjectTransform)
+    {
+        gameObjectTransform.gameObject.GetComponent<TextMeshProUGUI>().text = message;
+    }
+
+    void ShowMessage(int optionReferenceIndex)
+    {
+        Debug.Log("Seta para cima clicada + mensagem: "+ confirmationMessage.gameObject.GetComponent<TextMeshProUGUI>().text);
+        ChangeMessage("Deseja ir para " + optionReferences[optionReferenceIndex].gameObject.name+" ?", confirmationMessage);
+        option = optionReferenceIndex;
+        confirmation.gameObject.SetActive(true);
+    }
+
+    public void GoToRoom()
+    {
+        
+        switch (option)
+        {
+            case 0:
+                SceneManager.LoadScene("Scenes/Biblioteca");
+                break;
+            case 1:
+                SceneManager.LoadScene("Scenes/SalaDeAula");
+                break;
+            case 2:
+                SceneManager.LoadScene("Scenes/SalaProfessores");
+                break;
+            case 3:
+                SceneManager.LoadScene("Scenes/SalaRecursosMultifuncionais");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/KeyboardMappingLibrary.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class KeyboardMappingLibrary : MonoBehaviour
+{
+    public EventSystem m_EventSystem;
+    public Transform optionListBoxTransform;
+    public Transform originButton;
+
+    private List<Transform> optionList;
+    private int selectedIndex = 0, listLength;
+    //private Button btn;
+    private Color buttonDefault;
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionList = new List<Transform>();
+        foreach (Transform child in optionListBoxTransform.GetChild(0))
+        {
+            Debug.Log("Child: "+ child.name);
+            optionList.Add(child);
+        }
+        listLength = (optionList.Count - 1);
+        buttonDefault = optionList[selectedIndex].GetChild(1).GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("down"))
+        {
+            optionList[selectedIndex].GetChild(1).GetComponent<Image>().color = buttonDefault;
+            if (selectedIndex == listLength)
+                selectedIndex = 0;
+            else
+                selectedIndex++;
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            optionList[selectedIndex].GetChild(1).GetComponent<Image>().color = buttonDefault;
+            if (selectedIndex == 0)
+                selectedIndex = listLength;
+            else
+                selectedIndex--;
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            StartCoroutine("ReactivateButton");
+            transform.parent.gameObject.SetActive(false);   
+        }
+        
+        m_EventSystem.SetSelectedGameObject(optionList[selectedIndex].gameObject);
+        optionList[selectedIndex].GetChild(1).GetComponent<Image>().color = Color.black;
+    }
+
+    IEnumerator ReactivateButton()
+    {
+        m_EventSystem.SetSelectedGameObject(null);
+        yield return null;
+        m_EventSystem.SetSelectedGameObject(m_EventSystem.firstSelectedGameObject);
+    }
+    
+}
