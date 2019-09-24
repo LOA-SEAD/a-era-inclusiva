55343fff84a56508464c463f37371790bd7e0682
João Pedro Silva <pedrosgregorio9@gmail.com>
2019-09-24 18:20:19 -0300

    Listar acoes no HTPI

---
+++ Assets/ActionListWrapperHTPI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionListWrapperHTPI : MonoBehaviour
+{
+    private Animator _animator;
+    public SimpleScroll actionList;
+    public HTPIController controladorHTPI;
+    private List<String> _types;
+    public Button typeButtonPrefab;
+    private bool _loaded;
+    public GameObject Categories;
+
+    public Button acaoIconPrefab;
+    // Start is called before the first frame update
+    public void Show()
+    {
+        _animator.SetTrigger("Show");
+    }
+
+    public void Hide()
+    {
+        _animator.SetTrigger("Hide");
+    }
+
+    public void ShowActions(string tipo)
+    {
+        foreach (Transform child in actionList.parent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        actionList.UpdateChildrenCount();
+        var buttonList = new List<GameObject>();
+        foreach (var acao in GameManager.PlayerData.SelectedActions.Where(x=>x.tipo == tipo))
+        {
+            var button = Instantiate(acaoIconPrefab);
+            button.GetComponentInChildren<TextMeshProUGUI>().SetText(acao.icone + " " + acao.nome);
+            button.onClick.AddListener((() => controladorHTPI.SelectAction(acao)));
+            buttonList.Add(button.gameObject);
+        }
+        actionList.AddList(buttonList);
+        _animator.SetTrigger("Actions");
+    }
+
+    public void Return()
+    {
+        _animator.SetTrigger("Return");
+    }
+
+    public void Start()
+    {
+        _types = new List<string>();
+        _animator = GetComponent<Animator>();
+
+        
+    }
+
+    private void setCategories()
+    {
+        foreach (var acao in GameManager.GameData.Acoes)
+        {
+            if (!_types.Contains(acao.tipo))
+            {
+                _types.Add(acao.tipo);
+                var button = Instantiate(typeButtonPrefab, Categories.transform);
+                button.onClick.AddListener(() => ShowActions(acao.tipo));
+                button.GetComponentInChildren<TextMeshProUGUI>().SetText(acao.tipo);
+            }
+        }
+
+        _loaded = true;
+    }
+
+    public void Update()
+    {
+#if DEBUG
+        if (GameManager.PlayerData.SelectedActions.Count == 0)
+        {
+
+            (GameManager.GameData.Acoes).ForEach(x => GameManager.PlayerData.SelectedActions.Add(x));
+            
+        }
+#endif
+        if (!_loaded && GameManager.GameData.Loaded)
+        {
+            setCategories();
+        }
+    }
+}
--- Assets/HTPIController.cs
+++ Assets/HTPIController.cs
@@ -7,39 +7,62 @@ using UnityEngine.UI;
 
 public class HTPIController : MonoBehaviour
 {
-    private bool _loaded; 
-    public Button buttonPrefab;
+    private bool _loaded;
+    public BotaoDemandaHTPI buttonPrefab;
     public SimpleScroll demandList;
     private ClassDemanda _demanda;
+    private Dictionary<ClassDemanda, ClassAcao> _resolucoes;
+    private Dictionary<ClassDemanda, BotaoDemandaHTPI> _botaoPorDemanda;        
 
-    public SimpleScroll actionList;
+    public ActionListWrapperHTPI actionList;
 
     // Start is called before the first frame update
     void Awake()
     {
-        if(GameManager.GameData != null && GameManager.GameData.Demandas!=null && GameManager.GameData.Demandas.Count > 0)
+        _resolucoes = new Dictionary<ClassDemanda, ClassAcao>();
+        if (GameManager.GameData != null && GameManager.GameData.Demandas != null &&
+            GameManager.GameData.Demandas.Count > 0)
             Setup(null, EventArgs.Empty);
-         else {
+        else
+        {
             GameData.GameDataLoaded += Setup;
         }
     }
 
-    void Setup(object sender, EventArgs e) {
+    private void Start()
+    {
+        _botaoPorDemanda = new Dictionary<ClassDemanda, BotaoDemandaHTPI>();
+        actionList.gameObject.SetActive(false);
+        demandList.gameObject.SetActive(false);
+    }
+
+    void Setup(object sender, EventArgs e)
+    {
         var buttonList = new List<GameObject>();
-        foreach(ClassDemanda demanda in GameManager.GameData.Demandas) {
-            var button  = Instantiate(buttonPrefab);
-            button.GetComponentInChildren<TextMeshProUGUI>().text=demanda.descricao;
-            button.onClick.AddListener(()=>DemandSelected(demanda));
+        foreach (ClassDemanda demanda in GameManager.GameData.Demandas)
+        {
+            var button = Instantiate(buttonPrefab);
+            button.SetDemand(demanda);
+            button.GetComponent<Button>().onClick.AddListener(() => SelectDemand(demanda));
             buttonList.Add(button.gameObject);
-            
+            _botaoPorDemanda[demanda] = button;
         }
+
         demandList.AddList(buttonList);
     }
-    void DemandSelected(ClassDemanda demanda) {
-        _demanda = demanda;
-        
 
+    void SelectDemand(ClassDemanda demanda)
+    {
+        Debug.Log("1");
+        actionList.GetComponent<Animator>().SetTrigger("Show");
+        _demanda = demanda;
     }
+
+    public void SelectAction(ClassAcao acao)
+    {
+        _resolucoes[_demanda] = acao;
+        _botaoPorDemanda[_demanda].Select();
+    }
+
     // Update is called once per frame
-  
 }
